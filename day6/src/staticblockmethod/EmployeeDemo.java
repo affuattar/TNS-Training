1247e510ef57f4e5ef7c6509d56c60271fbcdcba
Affaan Attar <100771974+affuattar@users.noreply.github.com>
2025-04-28 19:26:56 +0530

    Add files via upload

---
+++ day6/src/staticblockmethod/EmployeeDemo.java
@@ -0,0 +1,11 @@
+package staticblockmethod;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		Employee e1=new Employee("Aniket", 1001);
+		System.out.println(e1);
+
+	}
+
+}
---
+++ day6/src/staticblockmethod/MyClassDemo.java
@@ -0,0 +1,21 @@
+package staticblockmethod;
+
+public class MyClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass.display();
+		
+		System.out.println();
+		
+		MyClass o1=new MyClass();
+		System.out.println(o1);
+		//  section 1  srno 1001
+		
+		
+		MyClass o2=new MyClass();
+		System.out.println(o2);
+
+	}
+
+}
