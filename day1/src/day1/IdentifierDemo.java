0b3edb8febb08a89190222862deeb9d0f95becbd
Affaan Attar <100771974+affuattar@users.noreply.github.com>
2025-04-28 19:22:22 +0530

    Add files via upload

---
+++ day1/src/day1/IdentifierDemo.java
@@ -0,0 +1,31 @@
+package day1;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//variable declaration - invalid
+		
+		//	int for =13;  keyword should not be as a variable
+			
+		//	int num 1=13; //keyword should have space
+			
+			//int @num3= 23;
+
+			
+			int $num1 = 23;
+			int num = 23;
+			
+			int FOR = 23;
+			
+			
+			long l = 234567890l;
+			System.out.println($num1);
+			
+			String studentName = "SHreya";
+			
+			System.out.println("Value is "+studentName);
+			
+	}
+
+}
---
+++ day1/src/day1/TypeCasting.java
@@ -0,0 +1,48 @@
+package day1;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//widening / implicit type casting 
+		
+		
+				byte b=10;
+				
+				int l=b;
+				
+				System.out.println(l);
+				
+				float f=100/3f;
+				
+				double d=f;
+				System.out.println(d);
+
+				char ch='A';
+				int i=ch;
+				
+				System.out.println(i);
+				
+				//narrowing /explicit type casting 
+				
+				double f1=10.52f;  // 8 byte in deciaml 
+				
+				long  ll=(long) f1;     // 8 byte in whole no
+				System.out.println(ll);
+				
+				long abc=9876543210L;
+				
+				int i12=(int) abc;
+				System.out.println(i12);
+				
+				
+				int xyz=130;
+				byte qwe=(byte) xyz;
+				
+				System.out.println(qwe);
+				
+				// byte range -128 to 0 to 127
+				
+	}
+
+}
